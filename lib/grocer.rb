21d442aaf833aa7368e5b9132fb66606c1f98c1f
Grant Hesketh <ghesketh76@gmail.com>
2020-12-02 20:24:22 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,6 +3,10 @@ require "pry"
 def find_item_by_name_in_collection(name, collection)
   find_item_by_name_in_collection.each do |item|
     if item[:item] == name
+      binding.pry
+      return item
+    end
+  end
 
 end
 
